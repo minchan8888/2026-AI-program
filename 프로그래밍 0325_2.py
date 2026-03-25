f1112cbc78c2aa3c2fac176a9e2d840065ed4203
unknown <coduadla1234@naver.com>
2026-03-25 16:47:32 +0900

    프로그래밍 0325.py

---
+++ 프로그래밍 0325_2.py
@@ -0,0 +1,9 @@
+score = [87,84,95,67,88,94,63]
+names = ['영호','순자','영식','순희',score]
+addressbook=[['영호',25,'010-0000-0000'],['철수',35,'010-0000-0000']]
+for i in addressbook:
+    print(i)  #i는 숫자열, 문자열 둘 다 받을 수 있음 -> 문자
+    
+
+ri = list(range(5))
+print(ri)
